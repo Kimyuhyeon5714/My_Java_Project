3fb20b7d9db0be17cf73d5b5156a67a29c1951df
User <User@DESKTOP-12F264H>
2020-12-29 20:31:56 +0900

    chapter03

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,6 +1,17 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
+	//동인천고등학교
+	//집중이수제
+	//응용프로그래밍개발
+	//구병국
+	/*
+	 * Javaorograming
+	 * Version 0.1
+	 * 2020.12.29
+	 * 
+	 * */
+	
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
---
+++ src/icehs/science/chapter03/MyAgeTest.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter03;
+
+public class MyAgeTest {
+
+	public static void main(String[] args) {
+		int bornYear;
+		bornYear = 2004;
+		int bornMonth;
+		bornMonth = 6;
+		int bornDay = 28;
+		
+		System.out.println(bornYear);
+		System.out.println(bornMonth);
+		System.out.println(bornDay);
+	}
+
+}
