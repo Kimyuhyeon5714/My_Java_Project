d3bab7d9d15a1f6bfe1e8deb1f021838b795c205
User <User@DESKTOP-12F264H>
2020-12-30 20:28:45 +0900

    Chapter04

---
+++ src/icehs/science/chapter04/CalculatorTest.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter04;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		int number1 = 20;
+		int number2 = 7;
+		
+		System.out.println("µ¡¼À°á°ú:"+(number1+number2));
+		System.out.println("»¬¼À°á°ú:"+(number1 - number2));
+		System.out.println("°ö¼À°á°ú:"+(number1*number2));
+		System.out.println("³ª´°¼À °á°ú:"+(number1/number2));
+		System.out.println("³ª¸ÓÁö:"+(number1 % number2));
+
+	}
+
+}
