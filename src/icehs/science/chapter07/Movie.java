86e89ad3c524a156584f89a13628121586a88ded
82104 <82104@DESKTOP-DVN3CCD>
2021-01-08 00:38:27 +0900

    Chapter07

---
+++ src/icehs/science/chapter07/Movie.java
@@ -0,0 +1,27 @@
+package icehs.science.chapter07;
+
+public class Movie {
+		private String title;
+		private String genre;
+		String director;
+		
+		void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public Movie() {
+			
+		}
+		
+		public Movie(String title, String director) {
+			this.title = title;
+			this.director = director;
+		}
+		
+		public Movie(String title, String director, String genre) {
+			this(title, director);
+			this.genre = genre;
+		}
+	}
+
+
